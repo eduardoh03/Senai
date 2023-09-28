64d3e8ec703768902cb0cb7d7bbbafd0661132fa
Eduardo <eduhenriquesrs@gmail.com>
2023-09-28 11:22:24 -0300

    Centralizado as colunas.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from .views import create_user, entrar, sair
 
 urlpatterns = [
-
     path('create_user', create_user, name="create_user"),
     path('entrar', entrar, name="entrar"),
     path('sair', sair, name="sair"),
